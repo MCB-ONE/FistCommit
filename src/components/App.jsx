85b86c23258637c166f024247db511625adde29d
toni <toni.one.dev@gmail.com>
2021-12-17 13:16:51 +0100

    Primer commit git

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Estilos particulares de APP.jsx
+import '../styles/sass/App.scss';
+import Taskslist from './lists/TasksList';
+
+/**
+ * Componente APP
+ * @returns Componente principal de la aplicación
+ */
+const App = () => {
+    return (
+      <div>
+        <h1>Bienvenido al proyecto base de React</h1>
+        <h2>Estamos usando Webpack, Babel y Eslint</h2>
+        {/* Renderizamos nuestro componente TasksList */}
+        <Taskslist />
+      </div>
+    );
+};
+
+export default App;
